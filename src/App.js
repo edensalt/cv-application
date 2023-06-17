caadc459a615a6340a873243dc41937b938b86fc
edensalt <124738007+edensalt@users.noreply.github.com>
2023-06-16 21:20:18 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SectionHeaderRow extends Component {
+  render() {
+    const { category } = this.props;
+    return <h1>{category}</h1>;
+  }
+}
+
+class SectionRow extends Component {
+  render() {
+    const { company, title } = this.props;
+
+    return (
+      <div>
+        <h2>{company}</h2>
+        <p>{title}</p>
+      </div>
+    );
+  }
+}
+
+class Preview extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFormVisible: false,
+    };
+  }
+
+  render() {
+    const { input } = this.props;
+    const rows = [];
+    // let lastCategory = null;
+
+    input.forEach((section) => {
+      if (section.category === "experience") {
+        console.log(section.category);
+        rows.push(
+          <SectionHeaderRow
+            category={section.category}
+            key={section.category}
+          />
+        );
+        rows.push(
+          <SectionRow
+            company={section.company}
+            title={section.title}
+            key={section.company}
+          />
+        );
+      }
+      // lastCategory = section.category;
+    });
+
+    return <div>{rows}</div>;
+  }
+}
+
+const DATA = [
+  {
+    category: "contact",
+    name: "Eden B",
+    phone: "555-555-5555",
+    email: "eden@eden.com",
+  },
+  {
+    category: "education",
+    school: "Tulane",
+    degree: "Public Health",
+    startDate: "2014",
+    endDate: "2017",
+  },
+  {
+    category: "experience",
+    company: "Wheel",
+    title: "Operations Manager",
+    startDate: "2021",
+    endDate: "2023",
+  },
+  {
+    category: "experience",
+    company: "Ready",
+    title: "Talent Acquisition Manager",
+    startDate: "2017",
+    endDate: "2021",
+  },
+  {
+    category: "skills",
+    skills: "React.js, JavaScript, HTML, CSS",
+  },
+];
+
+class App extends Component {
+  render() {
+    return <Preview input={DATA} />;
+  }
 }
 
 export default App;
